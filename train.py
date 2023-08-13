92fdbf6e195476087ecb6b82dc1eca12e968eae6
Tetsutaka <d611030k@gmail.com>
2023-08-13 16:07:37 +0900

    support training using cfg files and to output log

--- train.py
+++ train.py
@@ -1,37 +1,72 @@
+import configparser
+import logging
 from ultralytics import YOLO
 
-def train_model(yaml_path, weight="yolov8n.pt", data_yaml="./data.yaml", epochs=200, imgsz=640):
-    model = YOLO(yaml_path).load(weight)
-    model.train(data=data_yaml, epochs=epochs, imgsz=imgsz)
+
+class ConfigLoader:
+    def __init__(self, config_path: str) -> None:
+        """
+        Initialize and load the config from the specified path.
+        
+        :param config_path: Path to the .ini configuration file.
+        """
+        self.path = config_path
+        self.config = configparser.ConfigParser()
+        # Attempt to read the config file
+        if not self.config.read(self.path, encoding="utf-8"):
+            raise FileNotFoundError(f"'{self.path}' file not found or failed to load!")
+
+    def get_config_sections(self) -> list:
+        """
+        Retrieve all section names from the configuration.
+        
+        :return: List of section names.
+        """
+        return self.config.sections()
+
+
+class TrainYOLO:
+    def __init__(self, config_path) -> None:
+        self.config = ConfigLoader(config_path)
+        self.set_logger()
+
+    def set_logger(self):
+        # Set logger
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+        file_handler = logging.FileHandler("sample.log")
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        
+    def train(self):
+        # load config sections
+        sections = self.config.get_config_sections()
+        settings = self.config.config
+        for section in sections:
+            yaml = settings[section]["model_yaml"]
+            initial_weight = settings[section]["initial_weight"]
+            data_yaml = settings[section]["data_yaml"]
+            epoch_num = int(settings[section]["epochs"])
+            image_size = int(settings[section]["image_size"])
+            try:
+                self.logger.info("Loading weight to start training")
+                model = YOLO(yaml).load(initial_weight)
+                self.logger.info("start training the model")
+                model.train(data=data_yaml, epochs=epoch_num, imgsz=image_size)
+            except BaseException as e:
+                self.logger.error(e)
+
 
 def main():
-    try:
-        train_model("yolov8n.yaml")
-    except BaseException as e:
-        print(e)
-
-# model = YOLO("yolov8n.yaml").load("yolov8n.pt")
-# model.train(data="./data.yaml", epochs=200, imgsz=640)
-# del model
-
-# try:
-#     model = YOLO("yolov8s.yaml").load("yolov8s.pt")
-#     model.train(data="./data.yaml", epochs=200, imgsz=640)
-#     del model
-# except BaseException as e:
-#     print(e)
+    yolo = TrainYOLO(config_path="config.ini")
+    yolo.train()
 
 
-# try:
-#     model = YOLO("yolov8x.yaml").load("yolov8x.pt")
-#     model.train(data="./data.yaml", epochs=200, imgsz=640)
-#     del model
-# except BaseException as e:
-#     print(e)
-
-# try:
-#     model = YOLO("yolov8s-seg.yaml").load("yolov8s.pt")
-#     model.train(data="./data.yaml", epochs=200, imgsz=640)
-#     del model
-# except BaseException as e:
-#     print(e)
+if __name__ == "__main__":
+    main()
